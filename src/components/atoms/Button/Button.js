18a567aebbdb8da7b7279d259757258f5f707be2
Ravi Basil <142879212+RaviBytesMagic@users.noreply.github.com>
2025-09-25 12:12:34 +0530

    fix: button issue

--- src/components/atoms/Button/Button.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Button as MUIButton } from '@mui/material';
-const AppButton = ({ label, ...props }) => {
-    return _jsx(MUIButton, { ...props, children: label });
-};
-export default AppButton;
