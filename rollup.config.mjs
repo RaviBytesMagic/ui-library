85a961a92e31c83533964a0e22852909fab69676
Ravi Basil <142879212+RaviBytesMagic@users.noreply.github.com>
2025-09-25 13:07:58 +0530

    fix: added test case failing

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,10 +4,13 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
 import svgr from '@svgr/rollup'
+import babel from '@rollup/plugin-babel'
 import packageJson from './package.json' assert { type: 'json' }
 
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+
 export default [
-  // JS bundle
+  // JS/TS bundle
   {
     input: 'src/index.ts',
     output: [
@@ -16,13 +19,29 @@ export default [
     ],
     external: ['react', 'react-dom'], // peer deps
     plugins: [
-      resolve(),
+      resolve({ extensions }),
       commonjs(),
       svgr(),
       postcss({ extract: true, minimize: true }),
-      typescript({ tsconfig: './tsconfig.json' })
-    ]
+      typescript({ tsconfig: './tsconfig.json', sourceMap: true }),
+      babel({
+        babelHelpers: 'bundled',
+        extensions,
+        exclude: 'node_modules/**',
+        presets: [
+          ['@babel/preset-env', { modules: false }],
+          ['@babel/preset-react', { runtime: 'automatic' }],
+          '@babel/preset-typescript'
+        ]
+      })
+    ],
+    onwarn(warning, warn) {
+      // optionally ignore leftover module directive warnings
+      if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return
+      warn(warning)
+    }
   },
+
   // Type declarations
   {
     input: './src/index.ts',
