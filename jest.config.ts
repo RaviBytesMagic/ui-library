4f8dca75e96a6892e7d234cc7911bd1a9606b764
Ravi Basil <142879212+RaviBytesMagic@users.noreply.github.com>
2025-09-25 12:20:42 +0530

    fix: added test case failing

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom']
+}
--- src/index.js
+++
@@ -1,5 +0,0 @@
-// Export theme
-export { default as theme } from './theme/theme';
-// Export all components
-export * from './components/atoms/Button';
-// Add more component exports as you create them
